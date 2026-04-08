9b8699f0990af6efdb31ca4ba32b58a211553f16
Joeri Hermans <joeri@peinser.com>
2026-04-09 01:31:15 +0200

    Update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 delay=5 # Wait for K8S stdout flush :)
 
-exec sanic corshub.bin.standalone:app \
+exec python -m sanic corshub.bin.standalone:app \
     --host 0.0.0.0 \
     --port "${CORSHUB_PORT:-8000}" \
     --single-process
